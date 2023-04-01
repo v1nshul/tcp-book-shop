068227785b1fb5ec6a7c65a7dd0cfa14af04b151
Vanshul Kumar <50542207+v1nshul@users.noreply.github.com>
2023-04-01 01:04:02 +0100

    purchase-option-added
    In this commit:
    1. purchase option to user has been provided. 
    2. discount implemented.
    2. some minor name changes for the columns of excel files.

--- client.py
+++ client.py
@@ -23,27 +23,50 @@ def echo_client(port):
     # Send data 
     try: 
         # Send data
-        area= input('Enter the area you want to walk:')
-        min_mil = input('Enter the minimum milage you want to walk:')
-        max_mil = input('Enter the maximum milage you want to walk:')
-        lvl_diff = input('Enter the level of difficulty you want to walk:')
+        area= input('Enter the area you want to walk: ')
+        min_mil = input('Enter the minimum milage you want to walk: ')
+        max_mil = input('Enter the maximum milage you want to walk: ')
+        lvl_diff = input('Enter the level of difficulty you want to walk: ')
 
         message = area + ',' + min_mil + ',' + max_mil + ',' + lvl_diff
         print ("Sending to Server: %s :" % message) 
         sock.sendall(message.encode('utf-8')) 
         # Look for the response 
-        amount_received = 0 
-        amount_expected = len(message) 
-        #while amount_received < amount_expected: 
-         #   data = sock.recv(1024) 
-          #  amount_received += len(data) 
-           # print ("Received from Server:", data.decode("utf-8"))
+        data = sock.recv(1024) 
+        print ("Received from Server:\n", data.decode("utf-8"))
+
+        q_to_buy = input('Do you want to buy any of the books? (y/n): ')
+
+        if q_to_buy == 'y':
+            book_title = input('Enter the name of the book you want to buy: ')
+            book_quantity = int(input('Enter the quantity: '))
+
+            # Send purchase request to the server
+            request = f"{book_title},{book_quantity}"
+            sock.sendall(request.encode('utf-8'))
+            # Receive invoice from the server
+            invoice_data = sock.recv(1024)
+            print ("Received bill from the Server:\n", invoice_data.decode("utf-8"))
+
+            confirm = input('confirm the purchase? (y/n): ')
+            if confirm == 'y':
+                print("Thank you for your purchase and have a nice day :)")
+                '''
+                sock.sendall('y'.encode('utf-8'))
+                # Receive confirmation from the server
+                confirmation = sock.recv(1024)
+                print ("Received confirmation from Server:\n", confirmation.decode("utf-8"))
+                '''
+            else:
+                print("Thanks for visiting our store")
+        else:
+            print("Thanks for visiting our store")
     except socket.error as e: 
         print ("Socket error: %s" %str(e)) 
     except Exception as e: 
         print ("Other exception: %s" %str(e)) 
     finally: 
-        print ("Closing connection to the server") 
+        print ("\nClosing connection to the server") 
         sock.close() 
      
 if __name__ == '__main__': 
--- server.py
+++ server.py
@@ -11,15 +11,33 @@ data_payload = 2048
 backlog = 5
 
 books_data = pd.read_excel('circular_walks.xlsx')
+price_data = pd.read_excel('books_cost.xlsx')
 
 def suggest_books(area, min_mil, max_mil, lvl_diff):
-    
     filtered_books = books_data[(books_data['Area'].str.lower() == area.lower()) & 
                                 (books_data['Distance'].astype(int) >= min_mil) & 
                                 (books_data['Distance'].astype(int) <= max_mil) &
                                 (books_data['Difficult'].str.lower() == lvl_diff.lower())]
-
+    
     return filtered_books
+def purchase(title, quantity):
+  
+    # Filter and get the price of the book
+    book = price_data[(price_data['Books'].str.lower() == title.lower())]
+
+    # Generate the invoice
+    price_per_book = book['Price'].values[0]
+    total_price = quantity * price_per_book
+
+    #applying 10% discount if total is more than 75
+    if total_price > 75:
+        discount = total_price * 0.1
+        total_price = total_price - discount
+        invoice = f"You got a 10% discount!\nBook Title: {title}\nQuantity: {quantity}\nPrice per book: {price_per_book}\nTotal price after discount: {total_price}"
+    else:
+        invoice = f"Book Title: {title}\nQuantity: {quantity}\nPrice per book: {price_per_book}\nTotal price: {total_price}"
+    return invoice
+
 
 def echo_server(port):
     """ A simple echo server """
@@ -32,10 +50,10 @@ def echo_server(port):
     # Listen to clients, backlog argument specifies the max no. of queued connections
     sock.listen(backlog) 
     while True: 
-        print ("Waiting to receive message from client")
+        print ("\nWaiting to receive message from client")
         client, address = sock.accept() 
         data = client.recv(data_payload)
-        print("Message Received form client: ",data)
+        print("\nMessage Received form client: ",data)
 
         if data:
             # Split the client input into category and author
@@ -47,17 +65,27 @@ def echo_server(port):
 
             # Call the suggest_books function to get book suggestions
             book_suggestions = suggest_books(area,min_mil,max_mil,lvl_diff)
-            # Send book suggestions back to client
-            #book_suggestions_js = book_suggestions.to_json(orient='records')
-           
-            #client.send(bytes(book_suggestions_js, 'utf-8'))
 
-            book_suggestions_str = book_suggestions.to_string(index=False)
-            client.send(bytes(book_suggestions_str, 'utf-8'))
-            #client.send(book_suggestions_str.encode())
-            print ("Data to Send to Client: \n%s " % book_suggestions_str)
-            #print ("sent %d bytes back to %s" % (len(book_suggestions), address))
+            if book_suggestions.empty:
+                book_suggestions_str = "No books found for the given input"
+                client.send(bytes(book_suggestions_str, 'utf-8'))
+            else:
+                book_suggestions_str = book_suggestions.to_string(index=False)
+                client.send(bytes(book_suggestions_str, 'utf-8'))
+                #print ("sent %d bytes back to %s" % (len(book_suggestions), address))
             
+            books_to_buy = client.recv(data_payload)
+            print("\nRequest for Purchase obtained from the client: ", books_to_buy)
+
+            if books_to_buy:
+                # Split the purchase request into title and quantity
+                title_quantity = books_to_buy.decode().split(',')
+                title = title_quantity[0]
+                quantity = int(title_quantity[1])
+
+                # Call the purchase function to generate the invoice
+                invoice = purchase(title, quantity)
+                client.send(bytes(invoice, 'utf-8'))
         # End connection
         client.close() 
 
